7d535ec61cf41b5daf6aacd6082c65a77cebb2f5
yisimengba <465386131@qq.com>
2020-02-09 13:15:16 +0800

    Initial Commit

---
+++ HotPot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotPot
+//
+//  Created by duanzengguang on 2020/2/9.
+//  Copyright © 2020 duanzengguang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
